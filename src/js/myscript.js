acdbf73dbae542011a5f1324f5770d6d9278c30e
KirillPronin19 <92797078+KirillPronin19@users.noreply.github.com>
2022-02-24 17:42:07 +0700

    JS калькулятор (прототип)

--- src/js/myscript.js
+++ src/js/myscript.js
@@ -3,11 +3,11 @@
 // import calc from '.modules/calc'
 
 // window.addEventListener('DOMContentLoaded', () => {
-// 	'use strickt'
+// });
+
 
 // 	calc('#tip', '#diz', '#adapt', '.price');
 
-// });
 
 
 // let windowWidth = location.href;
@@ -15,10 +15,14 @@
 
 // console.log(windowWidth);
 
-let book = {
-	name: "js",
-	age: 3,
-};
+let siteType = prompt("Тип сайта 1-Одностраничный, 2-Многостраничный, 3-Интернет-магазин");
 
+console.log(siteType);
 
-console.log(book.age);
+let design = prompt("Дизайн сайта 1-Ваш дизайн, 2-Индивидуальный дизан, 3-Без дизайна");
+
+console.log(design);
+
+let adaptability = prompt("Адаптивность 1-Не адаптивный, 2-Адаптивный под мобильные устройства, 3-адаптивный");
+
+console.log(adaptability);
