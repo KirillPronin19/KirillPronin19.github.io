47f48ed802b440f8f030bf6194add8a9d7f94291
KirillPronin19 <92797078+KirillPronin19@users.noreply.github.com>
2022-02-24 15:58:59 +0700

    проблема с кнопкой

---
+++ src/js/calc.js
@@ -0,0 +1,23 @@
+// const calc = (tip, diz ,adapt, price) => {
+// 	const tipBlock = document.querySelector(tip),
+// 			dizBlock = document.querySelector(diz),
+// 			adaptBlock = document.querySelector(adapt),
+// 			priceBlock = document.querySelector(price);
+
+// 	let sum = 0;
+
+// 	const calcFunc = () => {
+// 		sum = Math.round((+tipBlock.value) * (dizBlock.value) + (adaptBlock.value));
+// 	}; 
+
+// 	tipBlock.addEventListener('change', calcFunc0);
+// 	dizBlock.addEventListener('change', calcFunc0);
+// 	adaptBlock.addEventListener('change', calcFunc0);
+
+// };
+
+// export default calc;
+
+
+
+
---
+++ src/js/myscript.js
@@ -0,0 +1,24 @@
+"use strict "
+
+// import calc from '.modules/calc'
+
+// window.addEventListener('DOMContentLoaded', () => {
+// 	'use strickt'
+
+// 	calc('#tip', '#diz', '#adapt', '.price');
+
+// });
+
+
+// let windowWidth = location.href;
+
+
+// console.log(windowWidth);
+
+let book = {
+	name: "js",
+	age: 3,
+};
+
+
+console.log(book.age);
